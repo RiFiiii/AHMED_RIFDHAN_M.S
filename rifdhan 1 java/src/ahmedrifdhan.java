4ac4e74bcca0a45c074cc57d71a7d6fbb10b4e26
RiFiiii <98368410+RiFiiii@users.noreply.github.com>
2022-01-29 15:47:56 +0530

    Update ahmedrifdhan.java

--- rifdhan 1 java/src/ahmedrifdhan.java
+++ rifdhan 1 java/src/ahmedrifdhan.java
@@ -3,7 +3,7 @@ public class ahmedrifdhan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hello");//underline "he"
 		}
 	
 }
