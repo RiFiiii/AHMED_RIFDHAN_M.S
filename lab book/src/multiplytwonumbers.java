ccb7a12e717b5844f5a28aba4cf33a35206aaa02
marhy <marhy@RiFiiZaN>
2022-02-01 12:15:04 +0530

    output for multiply two value or two number

---
+++ lab book/src/multiplytwonumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class multiplytwonumbers {
+public static void main(string[]args) {
+	Scanner scan=new scanner(System.in);
+	System.out.println("enter value for i..:");
+	int i=scan.nextInt();
+	System.out.println("Enter value for j..:");
+	int j=scan.nextInt();
+	int mul=i*j;
+	System.out.println("The Mulitiplication of i and j..:+mul);
+}
+}
