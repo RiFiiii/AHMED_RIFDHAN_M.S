ff0d121d3eddad356850da890d50c58a4aa2781c
marhy <marhy@RiFiiZaN>
2022-02-05 11:47:01 +0530

    ascii value:104

---
+++ lab book/src/ASCIIvalue.java
@@ -0,0 +1,14 @@
+
+public class ASCIIvalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'h';
+		int ascii =ch;
+		//you can also cast char to int
+		int castAscii =(int) ch;
+		System.out.println("The ASCII value of " + ch + "is:"+ascii);
+		System.out.println("The ASCII value of " + ch + "is:"+castAscii);
+	}
+
+}
