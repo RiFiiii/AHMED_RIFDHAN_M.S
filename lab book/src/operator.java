8b250e40629759ba711a6fe6d44866270ba6846f
marhy <marhy@RiFiiZaN>
2022-02-08 12:02:27 +0530

    ternary operator

---
+++ lab book/src/operator.java
@@ -0,0 +1,29 @@
+
+public class operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubpublic class ConditionalDemo {
+			//Ternary Operator
+			
+			System.out.println(true ?"success":"failure");
+			
+			
+			
+			boolean boo=true;
+			String result=boo?"success":"failure";
+			
+			System.out.println(result);
+			
+			
+			
+			int num=100%20;
+			
+			boo=(num==0);//comparison operator - will return true or false
+			
+			result=boo?"Even number...":"Odd Number...";
+			
+			System.out.println(result);
+
+	}
+
+}
