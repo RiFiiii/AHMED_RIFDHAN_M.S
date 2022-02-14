1ec5fb91293c11637434390e47094b107483d4aa
marhy <marhy@RiFiiZaN>
2022-02-14 21:53:58 +0530

    swap two numbers

---
+++ lab book/src/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) { 
+		 float first = 2.50f, second = 4.50f; 
+		 System.out.println("--Before swap--"); 
+		 System.out.println("First number = " + first); 
+		 System.out.println("Second number = " + second); 
+		 // Value of first is assigned to temporary
+		float temporary = first; 
+		 // Value of second is assigned to first
+		first = second; 
+		 // Value of temporary (which contains the initial value of first) is assigned to second
+		second = temporary; 
+		 System.out.println("--After swap--"); 
+		 System.out.println("First number = " + first); 
+		 System.out.println("Second number = " + second); 
+		 } 
+		}
