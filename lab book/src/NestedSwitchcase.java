97953a2ce6572c786b8da751aa07748054781a21
marhy <marhy@RiFiiZaN>
2022-02-22 20:39:07 +0530

    nestedswitchcasedemonstration

---
+++ lab book/src/NestedSwitchcase.java
@@ -0,0 +1,25 @@
+
+public class NestedSwitchcase {
+
+	public static void main(String[] args)
+	{
+		int tech = 2; 
+		int course = 2; 
+		 
+		 switch(tech){ 
+		case 1: 
+		 System.out.println("python"); 
+		 break; 
+		 case 2: 
+		 switch(course){ 
+		case 1: 
+		 System.out.println("J2EE"); 
+		 break; 
+		 case 2: 
+		 System.out.println("advance java"); 
+		 } 
+		 } 
+
+	}
+
+}
