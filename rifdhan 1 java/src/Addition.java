ef7231a64ae82581d1cc46fba64b0ca884a68374
marhy <marhy@RiFiiZaN>
2022-02-25 10:23:09 +0530

    ownaddthreeormanynumbers

---
+++ rifdhan 1 java/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("enter three numbers");
+		int first=400;
+		int second=700;
+		int third=566;
+		System.out.println(first+""+second+""+third);
+//add three numbers
+		int sum=first+second+third;
+		System.out.println("the sum is:"+sum);
+	}
+
+}
