5b547d0e9ec198b6a7bed1a4e380eaf59272d280
marhy <marhy@RiFiiZaN>
2022-02-25 10:12:41 +0530

    sumofarray

---
+++ lab book/src/SumOfArrayValues.java
@@ -0,0 +1,14 @@
+
+public class SumOfArrayValues {
+
+	public static void main(String[] args) {
+	int[]arr = {10,20,30,40,50,20};
+	int sum =0;
+	//advance for loop
+	for(int num:arr) {
+		sum=sum+num;
+	}
+	System.out.println("sum of array elements is:"+sum);
+	}
+
+}
