807330408e49670a93a3576cf8d19b155b94b5bf
marhy <marhy@RiFiiZaN>
2022-02-26 10:51:38 +0530

    pyramidexample

---
+++ lab book/src/PyramidExample.java
@@ -0,0 +1,10 @@
+public class PyramidExample{
+	public static void main(String[]args) {
+		for (int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.println("5");
+			}
+			System.out.println();//new line
+		}
+	}
+}
