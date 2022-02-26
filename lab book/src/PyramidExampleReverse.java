1692dedd7d9d032a11c61a5091a0644564eb777d
marhy <marhy@RiFiiZaN>
2022-02-26 10:55:49 +0530

    pyramidexamplereverse

---
+++ lab book/src/PyramidExampleReverse.java
@@ -0,0 +1,15 @@
+
+public class PyramidExampleReverse {
+
+	public static void main(String[] args) {
+		int term=6;
+		for(int i=1;i<=term;i++) {
+			for(int j=term;j>=i;j--) {
+				System.out.println("pyramid reverse");
+			}
+			System.out.println();//new line
+		}
+
+	}
+
+}
