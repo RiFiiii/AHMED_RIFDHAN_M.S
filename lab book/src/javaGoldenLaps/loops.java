a6f886428e61f27457d8fd2ad3aaadf2fc13f7cf
marhy <marhy@RiFiiZaN>
2022-03-17 11:06:14 +0530

    LoopsUsingWhileDo-While

--- lab book/src/javaGoldenLaps/loops.java
+++ lab book/src/javaGoldenLaps/loops.java
@@ -3,10 +3,18 @@ package javaGoldenLaps;
 public class loops {
 
 	public static void main(String[] args) {
-	 while(true) {
-		 System.out.println("infinite loops");
+	 //while(true) {
+		//System.out.println("infinite loops");
+	 //do {
+		// System.out.println("infinite loops");
+	 //}while(true);
+		int n=10;
+		while(n>10) {
+			System.out.println("this loop does not run");
+		}
+		do {
+			System.out.println("this loop run atleast once..");
+		}while(n>10);
 	 }
-
-	}
-
 }
+
