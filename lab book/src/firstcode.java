f9fb9d3f8b23a15016d92a2e7ee4fd2f1f311bba
marhy <marhy@RiFiiZaN>
2022-04-11 14:51:58 +0530

    datatypesAndAddThem

---
+++ lab book/src/firstcode.java
@@ -0,0 +1,22 @@
+
+public class firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=256;
+		
+		char c=65535;
+		
+		double d=56.6d;
+		
+		float s=56.6f;
+		
+		boolean b=false;
+		boolean t=true;
+		short n=32767;
+		long l=872634887;
+		
+		System.out.println(n+l);
+	}
+
+}
