9018216d8bc5dbf9394d2f1bc2ce05b19f86e874
marhy <marhy@RiFiiZaN>
2022-06-01 08:52:48 +0530

    escapesequence

---
+++ rifdhan 1 java/src/EscapeSequenceDemo.java
@@ -0,0 +1,21 @@
+
+public class EscapeSequenceDemo {
+
+	public static void main(String[] args) {
+		 System.out.println("hello\"eve\"world");
+		 //using escape sequence backslash here
+		 System.out.println("hello\'eve\'world");
+		 //single quote acceptable
+		 System.out.println("hello\n eve\"world");
+		 //new line
+		 System.out.println("hello\n\teve\n\tworld");
+		 //new line + tab space
+		 System.out.println("hello\f world");
+		 //page.break
+		 System.out.println("hello\b\b\b world");
+		 System.out.println("hello\r world");
+		 //carriage return
+
+	}
+
+}
