9b32de7689e6fe7cdee10face6fb8bc1000614c9
marhy <marhy@RiFiiZaN>
2022-06-08 00:51:54 +0530

    classDemo1

---
+++ rifdhan 1 java/src/classDemo1.java
@@ -0,0 +1,13 @@
+//study tips 1.class  2.abstract class  3.interface
+
+
+public class classDemo1 {
+	}
+class redpaint{//classification class-strictly should be either individual or under a category
+}
+abstract class paint{//classifier class-strictly common noun
+}
+interface painter{//action class-strictly role-classifier class
+	
+}
+
