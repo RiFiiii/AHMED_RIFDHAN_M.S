8f6bc8547e3c1294b5ac38cc39cb587e0dbefc9f
marhy <marhy@RiFiiZaN>
2022-06-27 02:15:24 +0530

    intro_just_revise_it

---
+++ lab book/src/javaGoldenLaps/ClassDemo1.java
@@ -0,0 +1,19 @@
+package javaGoldenLaps;
+//introducing classes
+//methods and classes
+//inheritance
+//package and interfaces
+public class ClassDemo1 {
+
+	public static void main(String[] args) {
+	}
+	class RedPaint{//classification class - strictly should be either individual or under a category
+		
+
+	}
+	abstract class Paint{//classifier class -strictly common noun
+	}
+	interface painter{//action class - strictly role - classfier class
+	}
+
+}
