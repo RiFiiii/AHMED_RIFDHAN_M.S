b60e77e19871c7be2e7c9e2189084b34c4332c14
marhy <marhy@RiFiiZaN>
2022-06-30 15:16:49 +0530

    variablesBasic

---
+++ rifdhan 1 java/src/inheritance/VariableDemo.java
@@ -0,0 +1,41 @@
+package inheritance;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+	 //variable declaration
+		
+		int i;
+		String s;
+		
+		//
+		i=10;
+		s="rifdhan";
+		float f=1.3f;
+		float ff=20;
+		byte b=127;//to -128
+		short sss=02220;
+		char c='c';
+		char ccc=97;
+		boolean bb=true;//u cannot give 0 or 1
+		boolean bbb=false;
+		
+		//valid identifiers or valid name
+		
+		int a;//valid identifier
+		/*
+		 * 2age - invalid identifier - because the name starts with a number
+		 * @age - invalid identifier - because the start with special character other than $ and _
+		 * $age - valid identifier
+		 * _age - valid identifier
+		 * a2ge - valid identifier - you can have number in between
+		 * a2g_e$ - valid identifier
+		 * ag e -invalid identifier - because there is a space
+		 * you cannot create a name which is a keyword or classname in java
+		 * the valid identifier concept applies to even the class names whuch you create. 
+		 */
+		
+
+	}
+
+}
