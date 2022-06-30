2a5ea159dbc0b7e1f41b54e7170acc221f435561
marhy <marhy@RiFiiZaN>
2022-06-30 15:18:32 +0530

    DataTypesAndVariables

---
+++ lab book/src/javaGoldenLaps/DataTypesAndVariables.java
@@ -0,0 +1,32 @@
+package javaGoldenLaps;
+
+public class DataTypesAndVariables {
+
+	public static void main(String[] args) {
+		int age=21_474_83_645;// 32 bit
+		byte b=10;//8 bit
+		short s=100;//16 bit
+		long l=232323;//64 bit
+		/*
+		 * valid identifiers should follow the rules
+		 * 1.variables name cannot start with a number value
+		 * 2.variables names cannot have special characters except $ and _ but not any other special character
+		 * 4. you cannot have space also in between
+		  */
+		
+		float f=1.234343f;//32bit
+		double d=23.435;//64bit
+		
+		char c='a';
+		char cc=100;//in java character is internally stored as number
+		
+		boolean boo=true;
+		boolean boo2=false;
+		
+		String ss="hello world";
+		System.out.println(ss);
+		
+
+	}
+
+}
