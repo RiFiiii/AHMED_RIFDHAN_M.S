741a8f7191a281efc3c3f25c9231035b739b2172
marhy <marhy@RiFiiZaN>
2022-06-30 15:12:46 +0530

    main1

---
+++ lab book/src/javaGoldenLaps/Main1Demo.java
@@ -0,0 +1,19 @@
+package javaGoldenLaps;
+
+public class Main1Demo {
+
+	public static void main(String[] args) {
+		 int number =10;
+		 //check if number is greater than 0
+		 if (number >11) {
+		 System.out.println("the number is positive.");
+		 }
+		 //execute this block
+		 //if number is not greater than 0
+		 else {
+			 System.out.println("the number is not positive.");
+		 }
+System.out.println("the statement outside if .......else nlock");
+	}
+
+}
