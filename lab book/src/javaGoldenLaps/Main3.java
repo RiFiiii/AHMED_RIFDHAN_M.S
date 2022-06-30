2ccd12c834c25d253d0d6ff8c73e34b7b35bbf5f
marhy <marhy@RiFiiZaN>
2022-06-30 15:33:10 +0530

    mainBasic3

---
+++ lab book/src/javaGoldenLaps/Main3.java
@@ -0,0 +1,27 @@
+package javaGoldenLaps;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		 int number=0;
+		 //check if number is greater than 0
+		 if(number >0) {
+			 System.out.println("the number is positive.");
+		 }
+		 //check if number is less than 0
+		 else if(number < 0) {
+			 System.out.println("the number is negative.");
+		 }
+		 
+		 //checks if number is less than 0
+		 else if(number<0) {
+			 System.out.println("the number is negative.");
+		 }
+		 //if both condition is false
+		 else {
+			 System.out.println("the number is 0.");
+		 }
+
+	}
+
+}
