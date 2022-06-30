7a5d781fd336131044a2dd1066aba95ab28df7b4
marhy <marhy@RiFiiZaN>
2022-06-30 15:16:06 +0530

    BasiRevise2

---
+++ lab book/src/javaGoldenLaps/HelloWorld2.java
@@ -0,0 +1,17 @@
+package javaGoldenLaps;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		//  i want to give  a text input in static fashion
+		
+		String myname="rifdhan";//=(equal to)is a assignment operator
+		System.out.println("welcome to java...:"+myname);//+ is a concatenation operator
+		
+		int n1=10;
+		int n2=20;
+		System.out.println("the sum of two values...:"+(n1+n2));
+
+	}
+
+}
