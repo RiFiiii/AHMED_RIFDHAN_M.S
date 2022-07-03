a43c049a786130946ffb4d6e6b2d667083fe87f4
marhy <marhy@RiFiiZaN>
2022-07-03 18:42:06 +0530

    IfOnly

---
+++ lab book/src/javaGoldenLaps/IfStatement.java
@@ -0,0 +1,15 @@
+package javaGoldenLaps;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		 int number = 10;
+		 //checks if number is less than 0
+		 if(number <0) {
+			 System.out.println("the number is negative.");
+		 }
+		 System.out.println("statement outside if block");
+
+	}
+
+}
