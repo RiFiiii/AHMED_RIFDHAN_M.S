7765352c89d1c40cbd0fea56dad39184ee9a7a36
marhy <marhy@RiFiiZaN>
2022-07-04 23:26:13 +0530

    TernaryRevise

---
+++ rifdhan 1 java/src/TernaryOperator.java
@@ -0,0 +1,14 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		 char ans ='y';
+		 //(?) condition is checked
+		 //(:) value is returned based on the condition.If
+		 //it is true then the first expression is returned
+		 //otherwise the second one.
+		 String value = ans=='y'?"IT'S YES":"OH NO!!!";
+		 System.out.println(value);
+	}
+
+}
