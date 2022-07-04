abc98d586f7b831a1d6f6e2777ab9223cb1f6ed8
marhy <marhy@RiFiiZaN>
2022-07-05 01:27:59 +0530

    UpdateASCIIvalue

--- lab book/src/ASCIIvalue.java
+++ lab book/src/ASCIIvalue.java
@@ -3,7 +3,7 @@ public class ASCIIvalue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char ch = 'h';
+		char ch = 'j';
 		int ascii =ch;
 		//you can also cast char to int
 		int castAscii =(int) ch;
