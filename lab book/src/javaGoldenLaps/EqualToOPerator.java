004f79c586283af8bd318f93142c4ac5fdb18517
marhy <marhy@RiFiiZaN>
2022-07-04 23:24:25 +0530

    EqualToOperator

---
+++ lab book/src/javaGoldenLaps/EqualToOPerator.java
@@ -0,0 +1,12 @@
+package javaGoldenLaps;
+
+public class EqualToOPerator {
+
+	public static void main(String[] args) {
+		 char ans='y';
+		 if(ans=='y') {
+			 System.out.println("Its is Yes.");
+		 }
+	}
+
+}
