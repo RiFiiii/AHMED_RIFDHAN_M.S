42ef9376c207d89ec22bdbacbba9325e11742ff7
marhy <marhy@RiFiiZaN>
2022-07-04 23:25:02 +0530

    BasicRemainderoperator

---
+++ lab book/src/javaGoldenLaps/BasicRemainderOperator.java
@@ -0,0 +1,11 @@
+package javaGoldenLaps;
+
+public class BasicRemainderOperator {
+
+	public static void main(String[] args) {
+		 int a=6;
+		 System.out.println(a%4);//this operator returns the remainder value after division
+
+	}
+
+}
