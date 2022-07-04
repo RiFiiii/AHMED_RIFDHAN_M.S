dc09c7884de586285807c08f18038454df9a0144
marhy <marhy@RiFiiZaN>
2022-07-04 23:24:09 +0530

    BasicDivisionOperator

---
+++ lab book/src/javaGoldenLaps/BasicDivisionOPerator.java
@@ -0,0 +1,11 @@
+package javaGoldenLaps;
+
+public class BasicDivisionOPerator {
+
+	public static void main(String[] args) {
+		 int a=6;
+		 System.out.println(a/4);//this operator only returns the remainder
+
+	}
+
+}
