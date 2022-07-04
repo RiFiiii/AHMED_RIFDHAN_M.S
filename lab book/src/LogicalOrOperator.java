854196065674471271a7e03cc8fec9ffd2918d34
marhy <marhy@RiFiiZaN>
2022-07-04 23:25:49 +0530

    LogicalOrOPeratorDemo

---
+++ lab book/src/LogicalOrOperator.java
@@ -0,0 +1,11 @@
+
+public class LogicalOrOperator {
+
+	public static void main(String[] args) {
+	int x =10;
+	if(x==5|x==0) {
+		System.out.println("value of x can be divided by 10");
+	}
+	}
+
+}
