661bfa9578f556129d22d92a36b53231332b8e49
marhy <marhy@RiFiiZaN>
2022-07-04 23:30:46 +0530

    NotEqualTOoperatorDemo

---
+++ lab book/src/javaGoldenLaps/NotEqualToOPerator.java
@@ -0,0 +1,13 @@
+package javaGoldenLaps;
+
+public class NotEqualToOPerator {
+
+	public static void main(String[] args) {
+		 int value =8;
+		 if(value != 10) {
+			 System.out.println("value is not equal to 10");
+		 }
+
+	}
+
+}
