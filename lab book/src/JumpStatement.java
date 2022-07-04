cd0b4270e6f6962c22071b04df2c35b3e78a5dad
marhy <marhy@RiFiiZaN>
2022-07-04 23:23:36 +0530

    JumpStateMent

---
+++ lab book/src/JumpStatement.java
@@ -0,0 +1,16 @@
+
+public class JumpStatement {
+
+	public static void main(String[] args) {
+		 //break is a jump statement
+		
+		int count=5;
+		while(true) {
+			if(count==5) {
+				System.out.println("we will use break statement"+"to jump out of the while block");
+				break;
+			}
+		}
+	}
+
+}
