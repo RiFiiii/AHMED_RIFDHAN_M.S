795b2be22d576db0561abc81074c1b0969b1d492
marhy <marhy@RiFiiZaN>
2022-07-04 23:25:28 +0530

    LesserThanRelationalDemo

---
+++ rifdhan 1 java/src/LessFRomRelationalOperator.java
@@ -0,0 +1,18 @@
+
+public class LessFRomRelationalOperator {
+
+	public static void main(String[] args) {
+		 int a=5;
+		 int b=15;
+		 if(a<b) {System.out.println("a is less than b");}
+//java has 6 relational operator
+		 /*
+		  * 1.< Less Than Sign
+		  * 2.<= Less Than or Equal to Sign
+		  * 3.> greater Than Sign
+		  * 4.>=Greater Than or Equal to Sign
+		  * 5.== Equal to Sign
+		  * 6.!= not equal TO Sign*/
+	}
+
+}
