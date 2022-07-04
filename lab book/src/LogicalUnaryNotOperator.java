908aa1a1acff4200a02a7d238f3207aebce7e807
marhy <marhy@RiFiiZaN>
2022-07-04 23:26:30 +0530

    LogicalUnaryNotOperator

---
+++ lab book/src/LogicalUnaryNotOperator.java
@@ -0,0 +1,18 @@
+
+public class LogicalUnaryNotOperator {
+
+	public static void main(String[] args) {
+		 boolean flag=false;
+		 //here the value of the flag is false.but if block
+		 //is only executed when the condition is true.so
+		 //incase we can use unary not(!).this negates the
+		 //boolean value of the flag.Tf the value of flag is
+		 //false and used with unary not then it will return
+		 //true and vice versa
+		 if(!flag) {
+			 System.out.println("hello world.");
+		 }
+
+	}
+
+}
