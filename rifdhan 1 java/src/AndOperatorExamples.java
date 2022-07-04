b176206b49e6a798b434f35926af4a9c3bc1483f
marhy <marhy@RiFiiZaN>
2022-07-04 23:23:51 +0530

    AndOperatorDemo

---
+++ rifdhan 1 java/src/AndOperatorExamples.java
@@ -0,0 +1,20 @@
+
+public class AndOperatorExamples {
+
+	public static void main(String[] args) {
+		 char ans='y';
+		 int count =2;
+		 
+		 if(ans=='y' & count==0) {
+			 System.out.println("count is zero.");
+		 }
+		 if(ans=='y'&count==1) {
+			 System.out.println("count is one");
+		 }
+		 if(ans=='y'&count==2) {
+			 System.out.println("count is two");
+		 }
+
+	}
+
+}
