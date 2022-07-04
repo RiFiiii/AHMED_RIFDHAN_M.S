7471bdabdab08395d55e359a00b833bf7a2be2b2
marhy <marhy@RiFiiZaN>
2022-07-04 23:31:56 +0530

    StringConcatination

---
+++ lab book/src/javaGoldenLaps/StringConcatenation.java
@@ -0,0 +1,12 @@
+package javaGoldenLaps;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		 String firstname=" Ahmed";
+		 String secondname=" Rifdhan m.s";
+		 String name=firstname+ "" +secondname;
+		 System.out.println("I Am Full Stack Developer My Name is" +name);
+	}
+
+}
