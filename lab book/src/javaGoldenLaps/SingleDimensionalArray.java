a4db4d4dd467195e1727a9e3c5473cef9bda30bb
marhy <marhy@RiFiiZaN>
2022-07-05 19:32:45 +0530

    SingleDimensionalArray

---
+++ lab book/src/javaGoldenLaps/SingleDimensionalArray.java
@@ -0,0 +1,11 @@
+package javaGoldenLaps;
+
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		 long[] empsalaries=new long[] {10000,40000,50000};//index number start in array is zero
+	System.out.println("value of the third element in the array:"+empsalaries[2]);//in java array is declared as object	 
+
+	}
+
+}
