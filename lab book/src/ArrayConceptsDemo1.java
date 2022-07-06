a3d188507c6520e51a08113128a4018cde50ae9b
marhy <marhy@RiFiiZaN>
2022-07-06 11:37:49 +0530

    h

--- lab book/src/ArrayConceptsDemo1.java
+++ lab book/src/ArrayConceptsDemo1.java
@@ -4,13 +4,12 @@ public class ArrayConceptsDemo1 {
 	public static void main(String[] args) {
 		 //single Dimensional Array
 		//One row-MULTIPLE Columns
-		int[] arr=new int[] {90,7,86,5,'c',4};
+		int[] arr=new int[] {90,7,86,5,'c',4,76,5,4,5,643,45,4,3};
 		
 		char c[]= {'a','v','k',97};
 		char cc[]=new char[10];
 		
 		//or 
-		
 		int arr2[]= {12,43,54,65,76};
 		int arr3[]= new int[5];//by default zero will be assigned
 		//how to Access Array Values
