a15624f22e81338b4dce5b5a13d5b9e957cc8441
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-09-18 19:20:07 +0530

    add

---
+++ lab book/src/SmartGraduateAssignment/Addition.java
@@ -0,0 +1,20 @@
+package SmartGraduateAssignment;
+
+public class Addition {
+		public static void main(String[] args) {
+			System.out.println("Enter two Numbers");
+			
+			int first=100;
+			int second=200;
+			
+			System.out.println(first+" "+second);
+			
+			//add two numbers
+			
+			int sum= first + second;
+			
+			System.out.println("the sum is...:" +sum);
+			
+		}
+
+}
