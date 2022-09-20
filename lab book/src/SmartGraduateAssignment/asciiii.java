71e1543ac3eecb726708588780688721f8c8d091
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-09-20 15:48:02 +0530

    Ascii

---
+++ lab book/src/SmartGraduateAssignment/asciiii.java
@@ -0,0 +1,19 @@
+package SmartGraduateAssignment;
+
+public class asciiii {
+	
+	public static void main(String[] args) {
+		char ch='r';
+		
+		int ascii=ch;
+		
+		//you can also cast char to int
+		 
+		int CastAscii=(int)ch;
+		
+		System.out.println("The ASCII Value Of " + ch+"is :" + ascii);
+		
+		System.out.println("The ASCII Value Of " + ch+"is :" + CastAscii);
+	}
+
+}
