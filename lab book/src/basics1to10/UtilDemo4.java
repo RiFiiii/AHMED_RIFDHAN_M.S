61c2e41f6d9930629786ed2814a7f03b1db5fea7
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-11-12 02:42:32 +0530

    UtilDemo

---
+++ lab book/src/basics1to10/UtilDemo4.java
@@ -0,0 +1,21 @@
+package basics1to10;
+
+public class UtilDemo4 {
+public static void main(String[] args) {
+	
+	//Gandhi Code-traditional implements
+	
+//	Set<String>set=new TreeSet<String>(new MyComparator());
+	
+	//Rajiv Gandhi Code -anonymous inner class
+	
+	//set <String>set=new TreeSet<String>(new Comparator<String>()){
+//	@Override
+	
+//	public int compare(String o1,String o2) {
+//		return o@.compareTo
+//	}
+			
+	
+}
+}
