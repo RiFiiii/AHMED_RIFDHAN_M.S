fa34336ba4e0a4b2bbc349c32d660f7b4c5a0b93
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-11-12 02:37:41 +0530

    OperatorsDemo

---
+++ lab book/src/basics1to10/OperatorsDemo.java
@@ -0,0 +1,27 @@
+package basics1to10;
+
+public class OperatorsDemo {
+
+	
+	// Operators : There are four types Of Operators 1. Arithmetic Operator  2. Logical Operator  3. Conditional Operator  These Three are important because I Am Bussiness Developer and Application Developer
+	
+	//  4. Binary Operator - This is not Necessary for Me 
+	
+	//  Arithmetic Operator :  + , - , * , / ,% 
+	public static void main(String[] args) {
+		
+	
+	int a=10;
+	int b=2;
+	
+	System.out.println("Addition "+a+b);
+	
+	System.out.println("Subtraction "+(a-b));
+	
+	System.out.println("Multiplication "+(a*b));
+	
+	System.out.println("Divide.....Quotient "+(a/b));
+	
+	System.out.println("Modulas....Remainder "+(a%b));
+}
+}
