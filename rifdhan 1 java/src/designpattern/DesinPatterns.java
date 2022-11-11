4f1af6a7d71cabf52d3374b527532687bf0b55d0
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-11-12 03:31:18 +0530

    DesignPatternDifinition

---
+++ rifdhan 1 java/src/designpattern/DesinPatterns.java
@@ -0,0 +1,42 @@
+package designpattern;
+
+public class DesinPatterns {
+	
+	/*                      Design Patterns
+	 * 
+	 *  Design Patterns : By Definition Design Patterns are reusable Solutions to Commonly Occuring Problems
+	 *  ( in the Context of Software design) Design patterns are Started as the best practices 
+	 *  that were applied again and again to similar problems encountered in different contexts.
+	 *  they become popular after they were collected in a formalized form in the gang of four book in 1994.
+	 *  orginally published with c++ and smallTalk code samples Design Patterns are Very Popular in java and c#
+	 *  can be applied in all object oriented languages in functional languages like scala,certain patterns are not neccesary anymore.
+	 *   */
+	
+	
+	/*    There Are Three Types of Patterns
+	 * 
+	 *     1.Creational Patterns  (7).
+	 *     
+	 *     2. Behavioral Patterns (11).
+	 *     
+	 *     3. Structural Patterns (6)
+	 * */
+
+	// Singleton :
+	/*
+	 * Ensure that only one instance of a class is created and provide a global access point to the object
+	 * 
+	 * Objective 
+	 * 
+	 *  TO Ensure that the object creation of particular class is restricted to only one object
+	 *  
+	 *   Advantages 
+	 *   
+	 *   Multiple Object of Certain class Could be a problem , so we Dont allow Them to create Multiple Objects.
+	 *   
+	 *   Factory : 
+	 *   
+	 *   Simplified Versions of Factory Method
+	 *   create objects Without Exposing the instantiated logic to the client and refers to the newly Created Object through a common interface.
+	 *   */ 
+}
