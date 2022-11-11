2ce91a58295f79fe6005e80d4d125e6c5250d53e
ahmed rifdhan <98368410+RiFiiii@users.noreply.github.com>
2022-11-12 02:33:47 +0530

    ClassDemo

---
+++ lab book/src/basics1to10/ClassDemo.java
@@ -0,0 +1,18 @@
+package basics1to10;
+
+public class ClassDemo {
+
+}
+
+
+class RedPaint{  // Classification class - strictly should be either individual or under a category
+	
+}
+
+abstract class Paint{
+	 // classifier class - strictly common noun
+}
+
+interface Painter{
+	// Action class - strictly role -classifier class
+}
